0cc8f0ee5b5513948d2097907f09b317acf18ebd
rahulkai <33171980+rahulkai@users.noreply.github.com>
2017-11-08 22:41:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World test!'))
+app.get('/', (req, res) => res.send('Hello World test test!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
